ff000a5248dc894b7eb4c68aa30f3ae93c7939a3
KMnO4-zx <1021385881@qq.com>
2023-06-19 21:07:26 +0800

    p51 完成

---
+++ 07-from/src/01-双向数据绑定.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from "vue";
+
+let text = ref("默认值")
+
+function submitHandler() {
+  console.log(text.value);
+}
+
+</script>
+
+<template>
+  <h1>表单</h1>
+  <hr>
+  <form @submit.prevent="submitHandler">
+    <div>
+      <input type="text" @input="(event) => (text = event.target.value)" :value="text">
+    </div>
+    <div>
+      <input type="text" @input="(event) => (text = event.target.value)" :value="text">
+    </div>
+    <button>提交</button>
+  </form>
+</template>
+
+<style></style>
--- 07-from/src/App.vue
+++ 07-from/src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 
 let text = ref("默认值")
+let bool = ref(false)
 
 function submitHandler() {
   console.log(text.value);
@@ -13,11 +14,13 @@ function submitHandler() {
   <h1>表单</h1>
   <hr>
   <form @submit.prevent="submitHandler">
+    <div>信息<input type="text" v-model="text"></div>
+    <div>是否<input type="checkbox" v-model="bool"></div>
     <div>
-      <input type="text" @input="(event) => (text = event.target.value)" :value="text">
-    </div>
-    <div>
-      <input type="text" @input="(event) => (text = event.target.value)" :value="text">
+      爱好：
+      <input type="checkbox" name="hobby" value="足球">
+      <input type="checkbox" name="hobby" value="篮球">
+      <input type="checkbox" name="hobby" value="羽毛球">
     </div>
     <button>提交</button>
   </form>
